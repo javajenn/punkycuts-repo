f2fce3707deb18fa03b8e59337eb315813414c73
javajenn <85006724+javajenn@users.noreply.github.com>
2021-12-12 17:00:45 -0500

    static update

--- staticfiles/store_index/scripts.js
+++ staticfiles/store_index/scripts.js
@@ -12,16 +12,27 @@ function initMap() {
 //     title: 'My favorite place for coffee!'
 //   });
   let markers = [];
+  debugger;
   let img = {
       url: "https://cdn-icons-png.flaticon.com/512/3253/3253134.png",
       scaledSize: new google.maps.Size(50,50),
   };
+  const blackCoffeeInfo = new google.maps.InfoWindow({
+      content: "<div class='info'><h2>blaCk Coffee lounge</h2><p>Black-owned local coffee shop with delicious chai lattes!</p></div>",
+  });
   const blackCoffeeMarker = new google.maps.Marker({
       position: {lat:39.10482143497105,lng:-84.51705276441803},
       map,
       icon: img,
   });
   markers.push(blackCoffeeMarker);
+  blackCoffeeMarker.addListener('click', ()=>{
+      blackCoffeeInfo.open({
+        anchor: blackCoffeeMarker,
+        map,
+        shouldFocus: false,
+      });
+  });
   const pauseCincyMarker = new google.maps.Marker({
       position: {lat:39.133304, lng:-84.509005},
       map,
